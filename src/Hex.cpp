753f0c8d1c592d3ed9a7a7a419d489b964778faf
Matthew Dagger <daggermatt@ecs.vuw.ac.nz>
2024-11-29 20:35:05 +1300

    Finish base hex code

--- src/Hex.cpp
+++ src/Hex.cpp
@@ -1,4 +1,10 @@
-/// @brief Hextypes are genreated to handle vectors and both types of number
+#include <cassert>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+/// @brief Hextypes are genreated to both types of number
 /// @tparam Number 
 /// @tparam vector 
 template <typename Number>
@@ -9,61 +15,74 @@ struct _Hex {
     };
     
     _Hex(Number q_, Number r_, Number s_): v{ q_, r_, s_ } {
-        assert (q + r + s == 0)
+        assert(q + r + s == 0);
     };
+
     _Hex(Number q_, Number r_): v{ q_, r_, - q_ - r_ } {};
 
-    bool operator == (const _Hex& a) {
+    bool operator == (const _Hex& a) const {
         return a.q == q && a.r == r && a.s == s;
     }
 
-    bool operator != (const _Hex& a) {
-        return !(this == a)
+    bool operator != (const _Hex& a) const {
+        return !(*this == a);
     }
 
-    /// @brief Calculates the vector length of this hex (from 0,0,0 to q,r,s)
-    /// @return The length in hexes
-    Number get_length() {
-        return Number((abs(hex.q) + abs(hex.r) + abs(hex.s)) / 2);
+    _Hex operator + (const _Hex& a) const {
+        return _Hex( q + a.q, r + a.r, s + a.s );
     }
 
+    _Hex operator - (const _Hex& a) const {
+        return _Hex(q - a.q, r - a.r, s - a.s);
+    }
+
+    template <typename Scalar>
+    _Hex operator * (Scalar k) const {
+        return _Hex(q * k, r * k, s * k);
+    }
+
+    // @brief Calculates the vector length of this hex (from 0,0,0 to q,r,s)
+    // @return The length in hexes
+    Number get_length() const {
+        return Number((abs(q) + abs(r) + abs(s)) / 2);
+    }
+
+    // @brief Calculates the hex distance to another hex
+    // @param a The other hex
+    // @return The distance in hexes
+    Number distance_to(const _Hex& a) const {
+        return (*this - a).get_length();
+    }
+
+    // @brief Gets the neighbouring hex to this in a specific direction
+    // @param direction The direction to look for a neighbour in
+    // @return The neighbouring hex
+    _Hex get_neighbour(int direction) {
+        return *this + hex_direction(direction);
+    }
 
 };
 typedef _Hex<int> Hex;
 typedef _Hex<double> FractionalHex;
 
 
-// /// @brief Adds two hex positions together
-// /// @param a 
-// /// @param b 
-// /// @return 
-Hex hex_add(Hex a, Hex b) {
-    return Hex(a.q + b.q, a.r + b.r, a.s + b.s);
-}
+// Holds the directional vector for hexes
+const vector<Hex> hex_directions = {
+    Hex(1, 0, -1), Hex(1, -1, 0), Hex(0, -1, 1),
+    Hex(-1, 0, 1), Hex(-1, 1, 0), Hex(0, 1, -1)
+};
 
-/// @brief 
-/// @param a 
-/// @param b 
-/// @return 
-Hex hex_subtract(Hex a, Hex b) {
-    return Hex(a.q - b.q, a.r - b.r, a.s - b.s);
-}
-
-/// @brief Multiplies a hex by some scalar
-/// @param a 
-/// @param k The scalar to multiply by
-/// @return 
-Hex hex_multiply(Hex a, int k) {
-    return Hex(a.q * k, a.r * k, a.s * k);
-}
-
-int hex_distance(Hex a, Hex b) {
-    return hex_subtract(a, b).get_length();
+// Getter for directions
+Hex hex_direction(int direction) {
+    assert(0 <= direction <= 5);
+    return hex_directions[direction];
 }
 
 
-FractionalHex a = FractionalHex(1.0,2,3);
-Hex b = Hex(1,2,3);
-bool check = a == a;
+int main() {
+    FractionalHex a = FractionalHex(0, 0, 0);
+    Hex b = Hex(1, -2, 1);
+    bool check = a == a;
 
-int c = b.get_length();
+}
+
